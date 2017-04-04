44c58b5690a544cf3507524825c277f1e8c5036f
Jeff Alexander <puremachinery@gmail.com>
2017-04-03 23:57:26 -0700

    add ignore_donuts capability

--- levelmoney.py
+++ levelmoney.py
@@ -12,7 +12,7 @@ URL = 'https://2016.api.levelmoney.com'
 API_TOKEN = 'AppTokenForInterview'
 UID = int(1110590645)
 TOKEN = 'EADCE81EC0B2CE7112E93E8321A71C6B'
-
+DONUT_MERCHANTS = ['Krispy Kreme Donuts', 'Dunkin #336784']
 
 def api_call(endpoint, content):
     content.setdefault("args", {})
@@ -25,18 +25,19 @@ def api_call(endpoint, content):
                          ).json()
 
 
-def get_spent_and_income_by_month(transactions):
+def get_spent_and_income_by_month(transactions, ignore_donuts=False):
 	spent_and_income_by_month = {}
 	for t in transactions:
 		amount = t['amount']
 		amount_type = 'income' if amount > 0 else 'spent'
 		month = t['transaction-time'][:7]  # e.g. '2014-10-24T07:20:00.000Z'
 		
-		if month in spent_and_income_by_month:
-			spent_and_income_by_month[month][amount_type] += amount
-		else:
-			spent_and_income_by_month[month] = {'spent': 0, 'income': 0}
-			spent_and_income_by_month[month][amount_type] = amount
+		if not ignore_donuts or t['merchant'] not in DONUT_MERCHANTS:
+			if month in spent_and_income_by_month:
+				spent_and_income_by_month[month][amount_type] += amount
+			else:
+				spent_and_income_by_month[month] = {'spent': 0, 'income': 0}
+				spent_and_income_by_month[month][amount_type] = amount
 	return spent_and_income_by_month
 
 
@@ -60,17 +61,27 @@ def format_financial_stats(spent_and_income_by_month):
 	
 
 if __name__ == "__main__":
+	import argparse
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--ignore_donuts', dest='ignore_donuts', action='store_true')
+	parser.set_defaults(ignore_donuts=False)
+	args = parser.parse_args()
+
+	print args.ignore_donuts
+
 	# load a user's transactions
 	response = api_call('/api/v2/core/get-all-transactions', {})
 
 	# determine how much money the user spends and makes each month
-	spent_and_income_by_month = get_spent_and_income_by_month(response['transactions'])
+	spent_and_income_by_month = get_spent_and_income_by_month(transactions=response['transactions'],
+															  ignore_donuts=args.ignore_donuts)
 
 	# determine how much money the user spends and makes on average
 	spent_and_income_by_month['average'] = get_average_spent_and_income(spent_and_income_by_month)
 
 	# output these numbers in the following format: {"2014-10": {"spent": "$200.00", "income": "$500.00"}, [...]
 	pprint(format_financial_stats(spent_and_income_by_month))
+
 	
 
 # [{"amount":-34300,"is-pending":false,"aggregation-time":1412686740000,
